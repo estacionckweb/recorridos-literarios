166d638571f1dc5fcd8511a23318da393bac08cb
ojack <ojackson@gmail.com>
2019-03-16 11:45:38 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/recorridos-literarios/precache-manifest.08c8e60a2c2ef869757f919bcd5a8991.js"
+  "/recorridos-literarios/precache-manifest.b51bf551911a1f916cabbca671c7779e.js"
 );
 
 workbox.clientsClaim();
